9439869434c5a6fa3ec25e406cfb4131c136d2eb
OUMAYMA_BOURICH <36383216+bourichoumayma@users.noreply.github.com>
2020-03-25 18:12:58 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div><!-- .container -->
+<footer>
+
+  <div class="copy-text">
+    <?php echo esc_html(get_theme_mod('nl_copyright_text', 'Crafted by Huseyn A. / Proudly powered by WordPress')); ?>
+  </div><!-- .copy-text -->
+
+</footer>
+
+<?php wp_footer(); ?>
+
+</div><!-- #content -->
+
+</body>
+</html>
